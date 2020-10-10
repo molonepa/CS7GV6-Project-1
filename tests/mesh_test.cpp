9314c9984b2ff5a87ad754962ab0b134484f449a
molonepa <molonepa@tcd.ie>
2020-10-10 09:37:58 +0100

    add updated mesh test

--- tests/mesh_test.cpp
+++ tests/mesh_test.cpp
@@ -8,6 +8,13 @@
 int main() {
 	Window window(1280, 720, "Hello");
 
+	std::vector<Vertex> vertices;
+	vertices.push_back(Vertex(glm::vec3(-0.5, -0.5, 0.0)));
+	vertices.push_back(Vertex(glm::vec3(0.0, 0.5, 0.0)));
+	vertices.push_back(Vertex(glm::vec3(0.5, -0.5, 0.0)));
+
+	Mesh triangle = Mesh(vertices, indices);
+
 	while (window.isOpen()) {
 		window.update();
 	}
