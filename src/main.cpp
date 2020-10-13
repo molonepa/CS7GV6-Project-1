7bfbb466942ad705d874c51816c0af21212d962e
molonepa <molonepa@tcd.ie>
2020-10-13 17:38:57 +0100

    update main application

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include "window.hpp"
 #include "vertex.hpp"
+#include "mesh.hpp"
+#include "shader.hpp"
 
 int main() {
+	Window window(1280, 720, "Application");
+	Shader defaultShader = Shader();
+
+	std::vector<Vertex> vertices;
+	vertices.push_back(Vertex(glm::vec3(-0.5, -0.5, 0.0)));
+	vertices.push_back(Vertex(glm::vec3(0.0, 0.5, 0.0)));
+	vertices.push_back(Vertex(glm::vec3(0.5, -0.5, 0.0)));
+	std::vector<int> indices;
+	indices.push_back(0);
+	indices.push_back(1);
+	indices.push_back(2);
+
+	Mesh triangle = Mesh(vertices, indices);
+
+	while (window.isOpen()) {
+		window.clear(0.5f, 0.5f, 0.5f, 1.0f);
+		defaultShader.bind();
+		triangle.render(GL_TRIANGLES);
+		window.update();
+	}
 	return 0;
 }
--- src/shaders/default.vs
+++ src/shaders/default.vs
@@ -1,11 +1,12 @@
 #version 400
 
 in vec3 vert_position;
-in vec4 vert_colour;
+in vec2 vert_uv;
+in vec3 vert_normal;
 
 out vec4 colour;
 
 void main() {
 	gl_Position = vec4(vert_position, 1.0);
-	colour = vert_colour;
+	colour = vec4(0.25, 0.8, 0.6, 1.0);
 }
